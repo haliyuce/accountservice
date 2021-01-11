54d56b1f08b0c096536b8e1b91f0cba8560b46e3
Ali <haliyuce@gmail.com>
2021-01-11 14:06:14 +0100

    last commit: still work to do on test level

---
+++ src/main/java/com/bank/accountservice/AccountServiceApplication.java
@@ -0,0 +1,46 @@
+package com.bank.accountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+
+@SpringBootApplication
+@Configuration
+public class AccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountServiceApplication.class, args);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "AccountService REST API",
+                "basic Account service app",
+                "API TOS",
+                "Terms of service",
+                new Contact("Ali YUCE", "www.example.com", "myeaddress@company.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+}
